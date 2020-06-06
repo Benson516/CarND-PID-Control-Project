cbfd3fa9768de5466b7d8c832f059f6fde8cbc07
BensonHuang <BensonHuang@itri.org.tw>
2020-06-06 23:56:44 +0800

    First wrokable parameter set

--- src/main.cpp
+++ src/main.cpp
@@ -37,9 +37,9 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
-  double init_Kp = 1.0;
+  double init_Kp = 0.1;
   double init_Ki = 0.0;
-  double init_Kd = 0.0;
+  double init_Kd = 1.0;
   pid.Init(init_Kp, init_Ki, init_Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
